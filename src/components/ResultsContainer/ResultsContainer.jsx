09e9bd5dbb51aaf32d758aacc118dccb92041a83
Himanshu Kumar <himanshurhlk57@gmail.com>
2021-08-31 14:58:55 +0530

    Add the nameit project

--- src/components/ResultsContainer/ResultsContainer.jsx
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -10,8 +10,32 @@ const ResultsContainer = ({ suggestedNames }) => {
     return <div className="results-container">{suggestNameJsx}</div>;
 };
 
-
 export default ResultsContainer;
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import NameCard from '../NameCard/NameCard';
+// import './ResultsContainer.css';
+
+// const ResultsContainer = ({ suggestedNames }) => {
+//     const suggestNameJsx = suggestedNames.map((suggestedName) => {
+//         return <NameCard key={suggestedName} suggestedName={suggestedName} />;
+//     });
+
+//     return <div className="results-container">{suggestNameJsx}</div>;
+// };
+
+
+// export default ResultsContainer;
 
 
 
